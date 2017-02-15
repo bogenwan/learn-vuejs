cf989c8cc2c224dbe89c2d6ff317cacb9968952c
Johnny <po_bo@hotmail.com>
2017-02-15 09:51:30 -0800

    created repo, no initialize first push this existing project to repo

---
+++ server/server.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/../client'));
+
+app.listen(PORT, console.log('server connected'));
